d881ba69eabf193f0ce7b788abf28abddf94b0b6
Salwa <100188930+slwndrh@users.noreply.github.com>
2024-09-26 23:45:09 +0700

    Delete Project directory

--- Project/app/src/main/java/com/example/smartarcle/data/Motor.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartarcle.data
-
-data class Motor(
-    val alarm: String = "0",
-    val button: String = "0",
-    val latitude: String = "0.0",
-    val longitude: String = "0.0"
-)
--- Project/app/src/main/java/com/example/smartarcle/data/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.smartarcle.data
-
-data class User(val token: String, val userType: String)
